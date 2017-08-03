60346729e5a11eab447c42a40aefe67d4ac63aa0
Conner Charlebois <cdcharlebois@gmail.com>
2017-08-03 18:41:14 -0400

    sliding

--- src/MobileTabs/widget/MobileTabs.js
+++ src/MobileTabs/widget/MobileTabs.js
@@ -15,60 +15,86 @@ define([
     "dojo/html",
     "dojo/_base/event",
     "MobileTabs/lib/jquery-1.11.2",
+    "MobileTabs/lib/owl"
 
-], function (declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, _jQuery) {
+
+], function(declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, _jQuery, _owl) {
     "use strict";
 
     var $ = _jQuery.noConflict(true);
-
-    return declare("MobileTabs.widget.MobileTabs", [ _WidgetBase ], {
+    window.myjq = $;
+    return declare("MobileTabs.widget.MobileTabs", [_WidgetBase], {
 
 
         // Internal variables.
         _handles: null,
         _contextObj: null,
 
-        constructor: function () {
+        constructor: function() {
             this._handles = [];
         },
 
-        postCreate: function () {
+        postCreate: function() {
+            var self = this;
             logger.debug(this.id + ".postCreate");
+            // console.log(_owl);
+
         },
 
-        update: function (obj, callback) {
+        update: function(obj, callback) {
             logger.debug(this.id + ".update");
+
 
             this._contextObj = obj;
             this._updateRendering(callback);
         },
 
-        resize: function (box) {
-          logger.debug(this.id + ".resize");
+        resize: function(box) {
+            logger.debug(this.id + ".resize");
         },
 
-        uninitialize: function () {
-          logger.debug(this.id + ".uninitialize");
+        uninitialize: function() {
+            logger.debug(this.id + ".uninitialize");
         },
 
-        _updateRendering: function (callback) {
+        _updateRendering: function(callback) {
             logger.debug(this.id + "._updateRendering");
 
-            if (this._contextObj !== null) {
-                dojoStyle.set(this.domNode, "display", "block");
-            } else {
-                dojoStyle.set(this.domNode, "display", "none");
-            }
+            var ul = this.domNode.parentElement.querySelector('ul'),
+                tabs = ul.children;
+
+            ul.className += ' owl-carousel';
+
+            var tabsOwl = $('.owl-carousel');
+            Array.from(tabs).forEach(function(item) {
+                item.addEventListener('click', function() {
+                    tabsOwl.owlCarousel('to', item.dataset.id * 1);
+                })
+            });
+
+            tabsOwl.owlCarousel({
+                nav: false,
+                center: true,
+                items: 3,
+                loop: false,
+                margin: 10,
+                responsive: {
+                    900: {
+                        items: 5
+                    }
+                }
+            });
 
             this._executeCallback(callback);
         },
 
-        _executeCallback: function (cb) {
-          if (cb && typeof cb === "function") {
-            cb();
-          }
+        _executeCallback: function(cb) {
+            if (cb && typeof cb === "function") {
+                cb();
+            }
         }
     });
+
 });
 
 require(["MobileTabs/widget/MobileTabs"]);
